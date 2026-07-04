df9a1fd6d377b899b75e664cf0b7f90ba2a8be46
Kushal2205a <introvertedspacemine2@gmail.com>
2026-07-04 06:11:57 +0530

    fix: typescript module resolution

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/geist";
-import "@fontsource/geist-mono";
+import "@fontsource/geist/index.css";
+import "@fontsource/geist-mono/index.css";
 import "./index.css";
 import App from "./App";
 
