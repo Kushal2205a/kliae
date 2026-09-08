6c1dd2bbaea731333a3c49dd74196a2e39f3e0a4
Kushal Panchali <introvertedspacemine2@gmail.com>
2026-09-08 13:13:17 +0530

    feat(tauri): enable clipboard image reads

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -125,6 +125,7 @@ pub fn run() {
     }
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
